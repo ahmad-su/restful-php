151aa6cf9160cb6b0b4161d51cb53fe9f7122055
ahmad-su <33272992+ahmad-su@users.noreply.github.com>
2023-03-10 23:01:36 +0700

    [milestone 1] Implemented OOP [20%]

--- index.php
+++ index.php
@@ -2,27 +2,63 @@
 require_once __DIR__ . '/src/routes.php';
 
 set_exception_handler(function ($e) {
+  //debugging
+  // error_log($e->getMessage());
+
   $error_code = $e->getCode() ?: 500;
+  $body = json_encode(["error" => $e->getMessage()]);
+  $content_len = strlen($body);
+  header("Content-Length: $content_len");
   header("Content-Type: application/json", true, $error_code);
   echo json_encode(["error" => $e->getMessage()]);
 });
 
 $http_method = $_SERVER['REQUEST_METHOD'];
 $http_uri = $_SERVER['REQUEST_URI'];
-$data = ["health-check" => "OK, We are alive!"];
-$routes = ["/health-check" => ["GET" => "routes\get_health_check"]];
 
-if (!array_key_exists($http_uri, $routes)) {
-  throw new Exception("Unknown endpoint", 404);
-} else {
-  if (!array_key_exists($http_method, $routes[$http_uri])) {
-    throw new Exception("Method not allowed", 400);
-  } else {
-    header("Content-Type: application/json");
-    $routes[$http_uri][$http_method]();
+$server = new Server();
+$server->addRoute('GET', '/health-check', 'handlers\health_check');
+// $server->addRoute('POST', '/health-check', 'handlers\health_check');
+$server->serve($http_method, $http_uri);
+
+class Server
+{
+  public $routes;
+
+  public function __construct()
+  {
+    $this->routes = array();
+  }
+
+  public function addRoute(string $method, string $path, string $handler)
+  {
+    if (!array_key_exists($path, $this->routes)) {
+      $array = [$method => $handler];
+      $this->routes[$path] =  $array;
+    } else {
+      // $array = [$path => [$method => $handler]];
+      $this->routes[$path][$method] =  $handler;
+    }
+  }
+
+  public function serve(string $method, string $path)
+  {
+
+    if (!array_key_exists($path, $this->routes)) {
+      // var_dump($this->routes);
+      throw new Exception("Unknown endpoint", 404);
+    } else {
+      if (!array_key_exists($method, $this->routes[$path])) {
+        // var_dump($this->routes);
+        throw new Exception("Method not allowed", 400);
+      } else {
+        header("Content-Type: application/json");
+        // var_dump($this->routes);
+        $this->routes[$path][$method]();
+      }
+    }
   }
 }
-
 //TODO: 
 //1. Refactor APP to use OOP paradigm and MVC pattern
 //2. Prepare DB (will use PG)
